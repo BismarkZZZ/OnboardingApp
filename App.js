77148732e2f9a76f16e675df60dfe07a9ffeb41e
Tardieh <tardiehbismark@hotmail.com>
2021-04-14 10:12:40 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, View} from 'react-native';
+import AddCart from './components/AddCart';
+import OnlineShopping from './components/OnlineShopping';
+import PaymentSucessful from './components/PaymentSucessful';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        {/* <AddCart /> */}
+        {/* <OnlineShopping /> */}
+        <PaymentSucessful />
+
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
+
+
+export default App;
